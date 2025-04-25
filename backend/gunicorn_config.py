93833e6e0ef851e3608a974646b25653508543c9
rohin20 <rohinphukan@gmail.com>
2025-04-25 15:08:44 -0400

    prod

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,8 +1,9 @@
 import multiprocessing
+import os
 
 # Gunicorn config variables
 workers = multiprocessing.cpu_count() * 2 + 1
-bind = "0.0.0.0:8000"
+bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"  # Use PORT from environment or default to 8000
 keepalive = 120
 timeout = 120
 worker_class = "uvicorn.workers.UvicornWorker" 
