be9783a694f531bee9b59f42985c9dc11cde30b2
rohin20 <rohinphukan@gmail.com>
2025-04-25 14:57:23 -0400

    prod

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 # Gunicorn config variables
 workers = multiprocessing.cpu_count() * 2 + 1
-bind = "0.0.0.0:10000"
+bind = "0.0.0.0:8000"
 keepalive = 120
 timeout = 120
 worker_class = "uvicorn.workers.UvicornWorker" 
